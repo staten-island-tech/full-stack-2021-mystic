fc5177ed653d82f8814e4e68ff8b7d7e4ca42601
michael whalen <michael.whalen@sitechhs.com>
2020-12-21 12:12:34 -0500

    asdfasdfa

--- basics/src/views/Home.vue
+++ basics/src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <h1>Hello {{ student }}</h1>
+    <button v-on:click="changeStudent">Change Student</button>
+    <h3>{{ animal }}</h3>
+    <button @click="changeAnimal">Change Animal</button>
+    <ul>
+      <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data() {
+    return {
+      student: "Harry",
+      animals: ["horse", "savva", "pig", "Zebra", "lion"],
+      animal: "",
+    };
+  },
+  methods: {
+    changeStudent: function() {
+      this.student = "Charlene";
+    },
+    changeAnimal: function() {
+      let number = Math.floor(Math.random() * 5);
+      this.animal = this.animals[number];
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 3rem;
+}
+button {
+  font-size: 2rem;
+}
+</style>
