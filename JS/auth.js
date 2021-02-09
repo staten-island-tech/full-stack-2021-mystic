d4709dc69e21ab3e091c8f143f9ad131cb5329f8
jchen2021 <57500286+jchen2021@users.noreply.github.com>
2021-02-09 18:54:09 -0500

    signup+login+logout

---
+++ JS/auth.js
@@ -0,0 +1,42 @@
+//sign up
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    //get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    //sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred =>{
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+});
+
+//log out
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log("signed out");
+    });
+});
+
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred =>{
+        console.log(cred.user)
+        //close and reset
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    })
+    
+})
---
+++ JS/index.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  
+  });
