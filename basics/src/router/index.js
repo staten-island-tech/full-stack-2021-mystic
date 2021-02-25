e3ba917c9802b50c074aa22d14ae9c2f35dda84b
nselwa <50632491+nselwa@users.noreply.github.com>
2021-02-25 18:06:32 -0500

    Revert "Copying Jiayu's files"
    This reverts commit 1a10103757bafabb12a20a2e4d3cbdabe636c6e5.

--- basics/src/router/index.js
+++ basics/src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Login from "../views/login.vue";
-import Register from "../views/register.vue";
-import FindPassword from "../views/findPassword.vue";
-import Secret from "../views/secret.vue";
 
 Vue.use(VueRouter);
 
@@ -12,43 +8,30 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
-    path: "/login",
-    name: "Login",
-    component: Login
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: Register
-  },
-  {
-    path: "/findPassword",
-    name: "FindPass",
-    component: FindPassword
-  },
-  {
-    path: "/secret",
-    name: "Secret",
-    component: Secret
-  },
-  {
-    path: "/about",
-    name: "About",
+    path: "/Props",
+    name: "Props",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/Props.vue"),
+  },
+  {
+    path: "/Form",
+    name: "Form",
+
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Form.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
