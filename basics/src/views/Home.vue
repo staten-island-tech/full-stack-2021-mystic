1a10103757bafabb12a20a2e4d3cbdabe636c6e5
nselwa <50632491+nselwa@users.noreply.github.com>
2021-02-25 17:57:30 -0500

    Copying Jiayu's files

--- basics/src/views/Home.vue
+++ basics/src/views/Home.vue
@@ -1,61 +1,39 @@
 <template>
-  <div class="container">
-    <h1>Hello {{ student }}</h1>
-    <button v-on:click="changeStudent">Change Student</button>
-    <h3>{{ animal }}</h3>
-    <button @click="changeAnimal">Change Animal</button>
-    <ul>
-      <li v-for="animal in animals" :key="animal">{{ animal }}</li>
-    </ul>
-    <h2 v-if="isLoggedIn">Welcome</h2>
-    <h2 v-else>Please Login</h2>
-    <button v-if="isLoggedIn" @click="logout">Logout</button>
-    <button v-else @click="login">Login</button>
+  <div class="home">
+    
+      <div class="welcome-message">
+      <h1>
+        Welcome to Mystic
+      </h1>
+      <div>
+      <button class='btn-large loggedOut red lighten-3'> 
+          <router-link to="/login">Login</router-link>
+      </button>
+      <button class='btn-large loggedOut red lighten-3'> 
+          <router-link to="/register">Sign Up</router-link>
+      </button>
+      
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {},
-  data() {
-    return {
-      student: "Harry",
-      animals: ["horse", "savva", "pig", "Zebra", "lion"],
-      animal: "",
-      isLoggedIn: false,
-    };
-  },
-  methods: {
-    changeStudent: function() {
-      this.student = "Charlene";
-    },
-    changeAnimal: function() {
-      let number = Math.floor(Math.random() * 5);
-      this.animal = this.animals[number];
-    },
-    login: function() {
-      this.isLoggedIn = true;
-    },
-    logout: function() {
-      this.isLoggedIn = false;
-    },
-  },
-  created() {},
-  mounted() {
-    this.changeAnimal();
-  },
-  beforeCreate() {},
+  components: {
+    HelloWorld
+  }
 };
 </script>
 
-<style lang="scss" scoped>
-h1 {
-  font-size: 3rem;
-}
-button {
-  font-size: 2rem;
+<style lang="scss">
+
+
+.btn-large{
+  margin:2%;
 }
 </style>
