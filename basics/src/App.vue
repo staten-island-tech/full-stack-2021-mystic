5ee99cddd3518fb7c46214a001bcefdd3372920d
jbb3r <57500157+jbb3r@users.noreply.github.com>
2021-05-26 09:18:17 -0400

    sjdlgkah

--- basics/src/App.vue
+++ basics/src/App.vue
@@ -1,126 +1,98 @@
 <template>
-<html lang="en">
   <div id="app">
-    
-      <!-- <a href="#">
-        <img class="logo" src="../src/assets/logo.png" />
-      </a> -->
-     <nav class="nav-wrapper green lighten-4"> 
-       <p class="logo">Mystics</p>
+    <div class="nav-wrapper">
+      <a>
+      <router-link to="/"><img class="logo" src="../src/assets/logo.png" />
+      </router-link>
+      </a>
+      
       <ul class="wrapper">
-       <li>
-         <router-link class="routerLink" to="/">Home</router-link>
-       </li>
-      <li>
-        <router-link class="routerLink loginTag" to="/login">Login</router-link>
-      </li>  
-      <li  @click="logout" >
-        <a class="routerLink logoutTag">Logout</a>
-      </li>
-        
-<!-- v-if="userIsAuthenticated" -->
-      <li>
-      <router-link class="routerLink" to="/register">Register</router-link>
-      </li>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
       </ul>
-    </nav>
-    <router-view></router-view>
+    </div>
+    <router-view />
   </div>
-</html>
 </template>
 
 <style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Freckle+Face&family=New+Tegomin&family=Original+Surfer&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Original Surfer', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
+ 
   color: #2c3e50;
 }
-body {
-  background-image: url("../src/assets/school.jpg");
-  margin: 0;
+body{
+    background-image: url('../src/assets/front.jpg');
+    margin:0;
+    background-position: center;
+    background-attachment: fixed;
 }
 #nav {
   padding: 30px;
+
   a {
     font-weight: bold;
     color: #2c3e50;
+
     &.router-link-exact-active {
       color: #42b983;
     }
   }
 }
-.logo {
-  width: 5vw;
-  margin-left: 3%;
-  margin-top:0;
-  font-size: 3rem;
-  color:#312c50;
-  text-decoration: none; 
-  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  font-weight:bold;
-  align-items: center;
+
+.logo{
+  margin-top: 3%;
+    width: 8vw;
+    height: 7vh;
 }
-.nav-wrapper {
-  width: 100vw;
+
+.nav-wrapper{
+  width:100vw;
+  height: 9vh;
+  background-color: #2b2d42;
   display: flex;
   justify-content: space-between;
   flex-direction: row;
+  align-items: center;
 }
-.wrapper {
-  padding-right: 2%;
-  width: auto;
-  text-decoration: none; 
-  color: rgb(83, 62, 158);
-  font-weight: bold;
-  display:flex;
-  flex-direction: row;
+.wrapper{
+  padding-right:5%;
+  width:auto;
+  color:white;
+  font-size: 1.3rem;
+  
 }
-.routerLink{
-  color: black;
-  font-weight: bold;
-}
-a.logoutTag{
-  text-decoration: none;
-  color: black;
-}
+
 </style>
 
-
 <script>
-import M from "materialize-css";
-import firebase from "firebase";
-import "firebase/auth";
+import M from 'materialize-css';
+//import {db} from './main';
+
 export default {
-  data(){
+   data() {
     return {
-      Login:''
+      users: {}
     }
   },
-    mounted() {
-    M.AutoInit();
-    this.checkUser();
-  },
-  methods:{
-  checkUser:function(){
-     firebase.auth().onAuthStateChanged((user) => {
-      if (firebase.auth().currentUser) {
-      console.log("User logged in"+user.email);
-      document.querySelector(".loginTag").style.display="none";
-      }else{
-        document.querySelector(".logoutTag").style.display="none";
-        console.log("Please sign in")
+    mounted () {
+        M.AutoInit()
+    },
+    /* firebase: {
+    users: {
+      source: db.ref('users'),
+      // Optional, allows you to handle any errors.
+      cancelCallback(err) {
+        console.error(err);
       }
-    });
-  },
-  logout(){
-    firebase.auth().signOut().then(() => {
-      console.log(firebase.auth().currentUser.email+ " has signed out")
-      }).catch((error) => {
-        console.log(error.message)
-      });
-  }
+    }, 
+  }*/
 }
-}
+
 </script>
