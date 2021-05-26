0e2efe6778ba9fb8d437490e71e81edcf03ffff0
jchen2021 <jiayu.chen21@sitechhs.com>
2021-05-26 09:37:21 -0400

    Revert "npm run build"
    This reverts commit 9ca0dbd5a2ea53f47270bd3d59c7d8a7c9e99d76.

---
+++ basics/dist/precache-manifest.277e92a339bca9f6e27b75ced080fa63.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efb200bc4ddd8135006",
+    "url": "/css/app.26d3119d.css"
+  },
+  {
+    "revision": "f08c8482978b32953d87",
+    "url": "/css/chunk-vendors.d2a37789.css"
+  },
+  {
+    "revision": "762be7107f32ad73b5093bdd7d8cd473",
+    "url": "/img/animal-crossing-wallpaper-tempBackground.762be710.webp"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f67821309929950419bb9ac13838f692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c7ffd223440bf562f90",
+    "url": "/js/about.e42292d7.js"
+  },
+  {
+    "revision": "7efb200bc4ddd8135006",
+    "url": "/js/app.1861b56c.js"
+  },
+  {
+    "revision": "f08c8482978b32953d87",
+    "url": "/js/chunk-vendors.27807913.js"
+  },
+  {
+    "revision": "7388293fde7b87d498bd5199c8887951",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
